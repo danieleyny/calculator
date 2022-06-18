7a831e0e6c653b3285d4055e7256067bdf01989b
Daniel Eyny <danieleyny@gmail.com>
2022-06-18 08:40:12 +0300

    page scroll added

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,3 +102,21 @@ const init = () => {
 init();
 
 calculateBtn.addEventListener("click", init);
+
+
+
+
+
+
+/*=============== SCROLL UP ===============*/
+
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL UP End ===============*/
+
